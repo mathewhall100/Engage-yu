5bc14bbae54e4f4fd7482f1d3a438790f3f59814
mathewhall100 <mathew.hall@dr.com>
2018-08-08 23:53:30 -0400

    validation scripts added to patient_detailsSchema

--- models/schema/patient_details.js
+++ models/schema/patient_details.js
@@ -3,16 +3,71 @@ const Schema = mongoose.Schema;
 
 
 const patient_detailsSchema = new Schema({
-    hospital_id: { type: String, unique: true, required: [true, "No hospital ID" ],  },
-    firstname: { type: String, required: [true, "No patient first name"] }, 
-    lastname: { type: String, required: [true, "No patient last name"] },
-    dob:  { type: String, required: [true, "No patient date of birth"] },
-    email: { type: String, unique: true, required: [true, "No patient email address"] },
+    
+    hospital_id: { 
+        type: String, 
+        validate: {
+            validator: function(hospnum) {
+                let re = /^(?!\d\s+$)[a-zA-Z\d\s]+{2,12}$/
+                return re.test(hospnum);
+            },
+            message: props => `${props.value} is not a valid name!`
+        },
+        unique: true, 
+        required: [true, "No hospital ID" ],  },
+    
+
+    firstname: { 
+        type: String, 
+        validate: {
+            validator: function(name) {
+                let re = /^[a-zA-Z]{2,30}$/
+                return re.test(name);
+            },
+            message: props => `${props.value} is not a valid name!`
+        },
+        required: [true, "No patient first name"] }, 
+
+    lastname: { 
+        type: String, 
+        validate: {
+            validator: function(name) {
+                let re = /^[a-zA-Z]{2,30}$/
+                return re.test(name);
+            },
+            message: props => `${props.value} is not a valid name!`
+        },
+        required: [true, "No patient last name"] },
+
+    dob:  { 
+        type: String, 
+        validate: {
+            // mm/dd/yyyy or mm-dd-yyyy
+            validator: function(date) {
+                let re = /^(0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])[\/\-]\d{4}$/
+                return re.test(date);
+            },
+            message: props => `${props.value} is not a valid email address!`
+        },
+        required: [true, "No patient date of birth"] },
+
+    email: { 
+        type: String, 
+        unique: true,
+        validate: {
+            validator: function(email) {
+                let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                return re.test(email);
+            },
+            message: props => `${props.value} is not a valid email address!`
+        },
+        required: [true, "No patient email address"] },
+
     phone: { 
         type: String, 
         validate: {
-            validator: function(val) {
-                return /\d{3}-\d{3}-\d{4}/.test(val);
+            validator: function(phone) {
+                return /\d{3}-\d{3}-\d{4}/.test(phone);
             },
             message: props => `${props.value} is not a valid phone number!`
         },
