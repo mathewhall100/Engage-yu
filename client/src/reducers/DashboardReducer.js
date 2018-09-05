629d88725c4480a93256c64c2c2686c7903de280
ShiKwan <31040895+ShiKwan@users.noreply.github.com>
2018-09-05 12:26:59 -0400

    more conflicts solved.

--- client/src/reducers/DashboardReducer.js
+++ client/src/reducers/DashboardReducer.js
@@ -4,15 +4,8 @@ export default function(state = [], action) {
     switch (action.type) {
         case FETCH_ALL_PATIENTS:
             return [action.payload.data.patientList];
-<<<<<<< HEAD
         default :
             return state;    
     }
     
 }
-=======
-        default:
-            return state;
-    }
-};
->>>>>>> master
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,13 +2,9 @@ import { combineReducers } from 'redux';
 import {  routerReducer } from "react-router-redux";
 import { reducer as formReducer } from 'redux-form';
 
-<<<<<<< HEAD
 import AuthReducer from './modules/auth';
 import PatientReducer from './PatientReducer';
 import UserReducer from './UserReducer';
-=======
-import AuthReducer from './AuthReducer';
->>>>>>> master
 import DashboardReducer from './DashboardReducer';
 import ConsoleTitleReducer from './ConsoleTitleReducer';
 
@@ -16,12 +12,7 @@ export default combineReducers({
     form: formReducer,
     auth : AuthReducer,
     router : routerReducer,
-<<<<<<< HEAD
     user: UserReducer,
-    patients: DashboardReducer
-=======
-    
     patients: DashboardReducer,
     consoleTitle: ConsoleTitleReducer
->>>>>>> master
 });
