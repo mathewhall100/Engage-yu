92c6a8c595aebb70230b0f2fc2c1b0f1cd275cf7
ShiKwan <31040895+ShiKwan@users.noreply.github.com>
2018-09-05 12:48:13 -0400

    moved appbar to the outer branch.

--- client/src/components/Routes.js
+++ client/src/components/Routes.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import { connect } from 'react-redux';
 import { login, logout, handleAuthentication, isAuthenticated, getProfile } from '../actions/AuthAction';
-
+import AppBar from './AppBar';
 import Homepage from './Homepage';
 import Admin from './Admin';
 import Callback from './Callback';
@@ -17,10 +17,12 @@ class Routes extends Component {
         console.log("routes props : " , this.props);
         return(
             <div className="App">
-
+                <AppBar/>
                 <Switch>
                     <Route exact path="/" render={props => <Homepage {...this.props}> </Homepage>} />    
                     <Route path='/admin' render={props => <Admin {...this.props}></Admin>} />
+                    <Route path='/admin/physician' render={props => <Admin {...this.props}></Admin>} />
+                    <Route path='/admin/medication' render={props => <Admin {...this.props}></Admin>} />
                     <Route exact path='/callback' render={props => <Callback></Callback>} />
                     <Route exact path="/sk" render={props => <SK {...this.props} title='Dashboard' ></SK>} />
                     <Route exact path='/sk/survey' render={props => <SK {...this.props}  title='Survey' ></SK>} />
